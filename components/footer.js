fa7340f765bf6b61e3954a74ef5d58bce340b21c
Jaanuja Koneswaran <nithujaanu29@gmail.com>
2024-05-17 09:06:12 +0530

    Second commit

---
+++ components/footer.js
@@ -0,0 +1,7 @@
+export default function Header(){
+    return <footer className="fixed w-full insert-x-0 bottom-0 flex flex-col items-center bg-neutral-900 text-center text-white">
+        <div className="w-full p-4 text-center" style={{ backgroundColor:'rgba(0,0,0,0.2)' }}>
+            copyright 2024
+        </div>
+    </footer>
+}
